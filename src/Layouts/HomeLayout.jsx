79b6fe85b3182713293aa064bff144504e5c4af7
Sanket Singh <singhsanket143@gmail.com>
2023-08-31 00:12:51 +0530

    Added logout functionality

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
 import Footer from '../Components/Footer';
+import { logout } from '../Redux/Slices/AuthSlice';
 function HomeLayout({ children }) {
 
     const dispatch = useDispatch();
@@ -28,11 +29,11 @@ function HomeLayout({ children }) {
         drawerSide[0].style.width = '0';
     }
 
-    function handleLogout(e) {
+    async function handleLogout(e) {
         e.preventDefault();
 
-        // const res = await dispatch(logout());
-        // if(res?.payload?.success)
+        const res = await dispatch(logout());
+        if(res?.payload?.success)
         navigate("/");
     }
 
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -38,6 +38,22 @@ export const login = createAsyncThunk("/auth/login", async (data) => {
     } catch(error) {
         toast.error(error?.response?.data?.message);
     }
+});
+
+export const logout = createAsyncThunk("/auth/logout", async () => {
+    try {
+        const res = axiosInstance.post("user/logout");
+        toast.promise(res, {
+            loading: "Wait! logout in progress...",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to log out"
+        });
+        return (await res).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
 })
 
 const authSlice = createSlice({
@@ -45,13 +61,20 @@ const authSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-        builder.addCase(login.fulfilled, (state, action) => {
+        builder
+        .addCase(login.fulfilled, (state, action) => {
             localStorage.setItem("data", JSON.stringify(action?.payload?.user));
             localStorage.setItem("isLoggedIn", true);
             localStorage.setItem("role", action?.payload?.user?.role);
             state.isLoggedIn = true;
             state.data = action?.payload?.user;
             state.role = action?.payload?.user?.role
+        })
+        .addCase(logout.fulfilled, (state) => {
+            localStorage.clear();
+            state.data = {};
+            state.isLoggedIn = false;
+            state.role = "";
         })
     }
 });
